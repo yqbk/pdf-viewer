b229643dd5ca187286884d9f083641d2848c67cf
Jakub Syrek <jksyrek@gmail.com>
2025-07-15 12:16:42 +0200

    Add test pdf.

--- src/app/HomeContent.tsx
+++ src/app/HomeContent.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { getDocument } from "./pdfjsConfig";
 
 const Page = styled.div`
   min-height: 100vh;
@@ -16,11 +18,34 @@ const Main = styled.main`
   justify-content: center;
 `;
 
+const Title = styled.h2`
+  margin-top: 20px;
+`;
+
 export default function HomeContent() {
+  const [numPages, setNumPages] = useState<number | null>(null);
+
+  useEffect(() => {
+    const loadPdf = async () => {
+      if (getDocument) {
+        const loadingTask = getDocument("public/pan_tadeusz.pdf");
+        const pdf = await loadingTask.promise;
+        setNumPages(pdf.numPages);
+      }
+    };
+    loadPdf();
+  }, []);
+
   return (
     <Page>
       <Main>
         <p>main</p>
+        <Title>PDF Example</Title>
+        {numPages !== null ? (
+          <p>Number of pages: {numPages}</p>
+        ) : (
+          <p>Loading PDF...</p>
+        )}
       </Main>
     </Page>
   );
