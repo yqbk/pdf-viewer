6f8348f6bb1de355b2ac6b6d2fd6550e7e45a5c1
Jakub Syrek <jksyrek@gmail.com>
2025-07-15 16:13:00 +0200

    Cleanup.

---
+++ src/app/styles.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Page = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Main = styled.main`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.h2`
+  margin-top: 20px;
+`;
